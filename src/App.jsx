f3f705ca321633665c6bf46541810d49577c6b87
matt secondi <matt_secondi@hotmail.com>
2025-05-24 11:50:31 -0400

    established base css structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./styles/App.scss"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/Home.jsx"
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
+import "./Nav.scss"
 
 const Nav = () => {
     const navigate = useNavigate();
     return (
         <>  
             <nav className="main-nav">
-                <div className="main-nav__logo">
+                <h2 className="main-nav__logo">
                     Breathe
-                </div>
+                </h2>
                 <ul className="main-nav__list">
                     <li className="main-nav__list-item">
                         Home
